7cd5ec96e5d29d4ac64e8e341b5dfaab21fa6328
Rémi Rérolle <remi.rerolle@gmail.com>
2015-03-05 12:11:48 +0100

    Add capabilities to the admin keyring

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,11 +15,15 @@ cat << EOF > /etc/ceph/ceph.conf
         filestore xattr use omap = true
         osd pool default size = 2
         osd pool default min size = 1
-        osd pool default pg num = 333
-        osd pool default pgp num = 333
 EOF
 
 ceph-authtool --create-keyring /tmp/ceph.mon.keyring --gen-key -n mon. --cap mon 'allow *'
+
+# ceph-authtool --create-keyring /etc/ceph/ceph.client.admin.keyring \
+#               --gen-key -n client.admin --set-uid=0 \
+#               --cap mon 'allow *' --cap osd 'allow *' --cap mds 'allow'
+
+# ceph-authtool /tmp/ceph.mon.keyring --import-keyring /etc/ceph/ceph.client.admin.keyring
 
 monmaptool --create --add $hostname $ipaddr --fsid $fsid /tmp/monmap
 
@@ -29,12 +33,17 @@ ceph-mon --mkfs -i $hostname --monmap /tmp/monmap --keyring /tmp/ceph.mon.keyrin
 touch /var/lib/ceph/mon/ceph-$hostname/done
 touch /var/lib/ceph/mon/ceph-$hostname/sysvinit
 /etc/init.d/ceph start mon.$hostname
-while ! ceph mon stat; do
+while ! ceph mon stat 2>/dev/null; do
         sleep 1
 done
 
 echo "[client.admin]" > /tmp/ceph.admin.keyring
 grep "key = " /tmp/ceph.mon.keyring >> /tmp/ceph.admin.keyring
+/bin/echo -e "\tauid = 0" >> /tmp/ceph.admin.keyring
+/bin/echo -e "\tcaps mds = \"allow\"" >> /tmp/ceph.admin.keyring
+/bin/echo -e "\tcaps mon = \"allow *\"" >> /tmp/ceph.admin.keyring
+/bin/echo -e "\tcaps osd = \"allow *\"" >> /tmp/ceph.admin.keyring
+
 ceph auth import -i /tmp/ceph.admin.keyring
 mv /tmp/ceph.admin.keyring /etc/ceph/ceph.client.admin.keyring
 
